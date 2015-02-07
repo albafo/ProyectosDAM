3afdea9d62a8299ecc6181e116a10705a323dd3e
albafo <alvaro.bano.fos@gmail.com>
2015-02-07 03:27:59 +0100

    Algoritmo Cesar con clave

---
+++ CesarClave/src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String frase="Hoy no me puedo levantar";
+		String abcOrig="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		String clave="EXTREMADURA";
+		int k=7;
+		
+		//Quitamos las letras repetidas de la clave
+		for(int i=0; i<clave.length(); i++) {
+			for(int j=i+1; j<clave.length(); i++) {
+			
+			}
+		}
+	}
+
+}
